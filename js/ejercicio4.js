ff93785d8199634d5b2821544fae50c598b04c51
timo marcelo hernan <marcelohfrt@gmail.com>
2026-04-25 21:34:22 -0300

    ejercicio 4,8,11,14 terminados

---
+++ js/ejercicio4.js
@@ -0,0 +1,6 @@
+const base = 10;
+const altura = 5;
+const area = base * altura;
+const perimetro = 2 * (base + altura);
+console.log(`el area del rectangulo es ${area} cm²`);
+console.log(`el perimetro del rectangulo es ${perimetro} cm`)
